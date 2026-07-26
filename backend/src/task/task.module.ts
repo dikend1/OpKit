765bf788ab8c8159c2be7037dfe83dd566af8ff0
Dias Maksatov <maxatovdias@gmail.com>
2026-07-26 23:24:10 +0500

    Register RedisModule in task module imports

--- backend/src/task/task.module.ts
+++ backend/src/task/task.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { RedisModule } from '../redis/redis.module';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { TaskGateway } from './task.gateway';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RedisModule],
   controllers: [TaskController],
   providers: [TaskService, TaskGateway],
 })
