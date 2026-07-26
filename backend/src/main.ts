c629d29004bcc5cbfa2d27e5c6bbc2b6df427b79
Dias Maksatov <maxatovdias@gmail.com>
2026-07-26 22:42:41 +0500

    Enable CORS for frontend communication

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({ origin: '*' });
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
 
   await app.listen(3000);
